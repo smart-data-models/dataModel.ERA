534c0fa492b95f12db252a9f2166ddbb1e26ab70
Alberto Abella <alberto.abella@fiware.org>
2023-11-14 18:06:04 +0100

    test of SQL export

---
+++ MaximumSpeedAndCantDeficiency/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model MaximumSpeedAndCantDeficiency of the subject dataModel.ERA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE MaximumSpeedAndCantDeficiency_type AS ENUM ('MaximumSpeedAndCantDeficiency');
+CREATE TABLE MaximumSpeedAndCantDeficiency (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type MaximumSpeedAndCantDeficiency_type, vehicleTypeMaximumCantDeficiency NUMERIC, vehicleTypeMaximumSpeed NUMERIC);
