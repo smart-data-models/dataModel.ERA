b3886a26bc0b9789205da6c4a8507c493706786f
Alberto Abella <alberto.abella@fiware.org>
2023-11-14 18:05:54 +0100

    test of SQL export

---
+++ InfrastructureManager/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model InfrastructureManager of the subject dataModel.ERA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE InfrastructureManager_type AS ENUM ('InfrastructureManager');
+CREATE TABLE InfrastructureManager (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, definesSubset TEXT, description TEXT, id TEXT PRIMARY KEY, imCode TEXT, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type InfrastructureManager_type);
