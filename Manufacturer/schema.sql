6bdbf5142f23614a01919a4111b51565928d7dda
Alberto Abella <alberto.abella@fiware.org>
2023-11-14 18:06:01 +0100

    test of SQL export

---
+++ Manufacturer/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Manufacturer of the subject dataModel.ERA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Manufacturer_type AS ENUM ('Manufacturer');
+CREATE TABLE Manufacturer (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Manufacturer_type);
