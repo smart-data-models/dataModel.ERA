fddce3c6bed96cb4c29c61f21bac123c4eb310c3
Alberto Abella <alberto.abella@fiware.org>
2023-11-14 18:06:18 +0100

    test of SQL export

---
+++ RaisedPantographsDistanceAndSpeed/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model RaisedPantographsDistanceAndSpeed of the subject dataModel.ERA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE RaisedPantographsDistanceAndSpeed_type AS ENUM ('RaisedPantographsDistanceAndSpeed');
+CREATE TABLE RaisedPantographsDistanceAndSpeed (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, raisedPantographsDistance NUMERIC, raisedPantographsNumber NUMERIC, raisedPantographsSpeed NUMERIC, seeAlso JSON, source TEXT, type RaisedPantographsDistanceAndSpeed_type);
