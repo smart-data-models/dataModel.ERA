8f3466df04226fba23e4e093ed91228fbf4e35a7
Alberto Abella <alberto.abella@fiware.org>
2023-11-14 18:06:27 +0100

    test of SQL export

---
+++ SpecialTunnelArea/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model SpecialTunnelArea of the subject dataModel.ERA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE SpecialTunnelArea_type AS ENUM ('SpecialTunnelArea');
+CREATE TABLE SpecialTunnelArea (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type SpecialTunnelArea_type);
