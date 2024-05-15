1e800dafa2582ab4c7478208b5e7ef16ba7546a3
Alberto Abella <alberto.abella@fiware.org>
2024-05-16 01:34:04 +0200

    created example of code

---
+++ Track/code/code_for_using_dataModel.ERA_Track.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "Track"
+subject = "dataModel.ERA"
+IdPhoneErtmsRadioBlockCenter = "{'type': 'Property', 'value': 'Town myself religious would lose. Statement first ball experience threat. Oil position rule.'}"
+attribute = "IdPhoneErtmsRadioBlockCenter"
+value = IdPhoneErtmsRadioBlockCenter
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+accelerationLevelCrossing = "{'type': 'Property', 'value': 'Us day century either. Job just either group message left born. Idea wear produce two.'}"
+attribute = "accelerationLevelCrossing"
+value = accelerationLevelCrossing
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+additionalBrakingInformationDocument = "{'type': 'Property', 'value': 'Even product let cell last rule. Cultural require specific hold see police. Smile st'}"
+attribute = "additionalBrakingInformationDocument"
+value = additionalBrakingInformationDocument
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+atoErrorCorrectionsOnboard = "{'type': 'Property', 'value': 'Brother style drug place company PM. New past Democrat common piece reveal. Loss bed collection theory week netw'}"
+attribute = "atoErrorCorrectionsOnboard"
+value = atoErrorCorrectionsOnboard
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
