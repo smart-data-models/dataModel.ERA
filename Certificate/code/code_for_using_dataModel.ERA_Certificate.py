a5c407923197ed52e83ce66cb1d0cf84996e17a3
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:32:11 +0200

    created example of code

--- Certificate/code/code_for_using_dataModel.ERA_Certificate.py
+++ Certificate/code/code_for_using_dataModel.ERA_Certificate.py
@@ -24,13 +24,18 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Certificate"
 subject = "dataModel.ERA"
-state = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Certificate:state:HXAA:38754798'}"
+state = "urn:ngsi-ld:Certificate:state:KHMM:25466110"
 attribute = "state"
 value = state
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
