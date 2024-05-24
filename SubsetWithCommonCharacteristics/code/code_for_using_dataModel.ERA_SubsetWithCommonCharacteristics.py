838774145a6f37a89fc4f9cdae6e08a83a867330
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:33:43 +0200

    created example of code

--- SubsetWithCommonCharacteristics/code/code_for_using_dataModel.ERA_SubsetWithCommonCharacteristics.py
+++ SubsetWithCommonCharacteristics/code/code_for_using_dataModel.ERA_SubsetWithCommonCharacteristics.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "SubsetWithCommonCharacteristics"
 subject = "dataModel.ERA"
-IdPhoneErtmsRadioBlockCenter = "{'type': 'Property', 'value': 'Seem discussion realize almost detail cul'}"
+IdPhoneErtmsRadioBlockCenter = "Building none down investment possible member country."
 attribute = "IdPhoneErtmsRadioBlockCenter"
 value = IdPhoneErtmsRadioBlockCenter
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-accelerationLevelCrossing = "{'type': 'Property', 'value': 'Site gun if side population guess. Recent event green letter only learn safe. New ahead ask speak rule. Box this by catch leg hers'}"
+accelerationLevelCrossing = "World middle talk into understand paper."
 attribute = "accelerationLevelCrossing"
 value = accelerationLevelCrossing
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-additionalBrakingInformationDocument = "{'type': 'Property', 'value': 'Certain certainly treat population rich. Which those term'}"
+additionalBrakingInformationDocument = "Understand house degree though. System she science election."
 attribute = "additionalBrakingInformationDocument"
 value = additionalBrakingInformationDocument
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-atoErrorCorrectionsOnboard = "{'type': 'Property', 'value': 'Write language operation hundred show message specific '}"
+atoErrorCorrectionsOnboard = "Cover close yes simple entire."
 attribute = "atoErrorCorrectionsOnboard"
 value = atoErrorCorrectionsOnboard
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
