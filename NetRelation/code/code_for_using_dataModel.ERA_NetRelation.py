8f36a0de409bb95318bea2d66c239528076331f2
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:33:06 +0200

    created example of code

--- NetRelation/code/code_for_using_dataModel.ERA_NetRelation.py
+++ NetRelation/code/code_for_using_dataModel.ERA_NetRelation.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "NetRelation"
 subject = "dataModel.ERA"
-elementA = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:NetRelation:elementA:NXWD:06501472'}"
+elementA = "urn:ngsi-ld:NetRelation:elementA:TUIU:78345951"
 attribute = "elementA"
 value = elementA
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-elementB = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:NetRelation:elementB:CNDJ:23196407'}"
+elementB = "urn:ngsi-ld:NetRelation:elementB:DHHX:71184798"
 attribute = "elementB"
 value = elementB
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-navigability = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:NetRelation:navigability:TDBB:08079538'}"
+navigability = "urn:ngsi-ld:NetRelation:navigability:TZOM:33501230"
 attribute = "navigability"
 value = navigability
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-positionOnA = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:NetRelation:positionOnA:WRNS:94567510'}"
+positionOnA = "urn:ngsi-ld:NetRelation:positionOnA:YSDM:89916223"
 attribute = "positionOnA"
 value = positionOnA
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
