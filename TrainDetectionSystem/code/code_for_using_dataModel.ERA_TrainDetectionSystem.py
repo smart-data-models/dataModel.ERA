85ba1e1ee18c46dce5bb97fe616972d6d9e9bac1
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:33:57 +0200

    created example of code

--- TrainDetectionSystem/code/code_for_using_dataModel.ERA_TrainDetectionSystem.py
+++ TrainDetectionSystem/code/code_for_using_dataModel.ERA_TrainDetectionSystem.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "TrainDetectionSystem"
 subject = "dataModel.ERA"
-flangeLubeRules = {'type': 'Property', 'value': True}
+flangeLubeRules = False
 attribute = "flangeLubeRules"
 value = flangeLubeRules
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-maxDistConsecutiveAxles = {'type': 'Property', 'value': 181}
+maxDistConsecutiveAxles = 864
 attribute = "maxDistConsecutiveAxles"
 value = maxDistConsecutiveAxles
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-maxDistEndTrainFirstAxle = {'type': 'Property', 'value': 718}
+maxDistEndTrainFirstAxle = 864
 attribute = "maxDistEndTrainFirstAxle"
 value = maxDistEndTrainFirstAxle
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-maxFlangeHeight = {'type': 'Property', 'value': 8.4}
+maxFlangeHeight = 591.5
 attribute = "maxFlangeHeight"
 value = maxFlangeHeight
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
