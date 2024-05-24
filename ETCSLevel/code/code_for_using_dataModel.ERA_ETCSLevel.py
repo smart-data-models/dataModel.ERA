e4d10dbb9b817b4a47ad8e503311df751f85ab3d
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:32:19 +0200

    created example of code

--- ETCSLevel/code/code_for_using_dataModel.ERA_ETCSLevel.py
+++ ETCSLevel/code/code_for_using_dataModel.ERA_ETCSLevel.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ETCSLevel"
 subject = "dataModel.ERA"
-etcsBaseline = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:ETCSLevel:etcsBaseline:COZU:74311343'}"
+etcsBaseline = "urn:ngsi-ld:ETCSLevel:etcsBaseline:HAGI:99857296"
 attribute = "etcsBaseline"
 value = etcsBaseline
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-etcsLevelType = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:ETCSLevel:etcsLevelType:DFOI:51546732'}"
+etcsLevelType = "urn:ngsi-ld:ETCSLevel:etcsLevelType:VOLD:97362219"
 attribute = "etcsLevelType"
 value = etcsLevelType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
