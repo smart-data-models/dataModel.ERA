4e9d04fc0d6e1cfeeff7e94810c683a3ef27d97c
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:33:10 +0200

    created example of code

--- OperationalPoint/code/code_for_using_dataModel.ERA_OperationalPoint.py
+++ OperationalPoint/code/code_for_using_dataModel.ERA_OperationalPoint.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "OperationalPoint"
 subject = "dataModel.ERA"
-digitalSchematicOverview = "{'type': 'Property', 'value': 'Try somebody term.'}"
+digitalSchematicOverview = "Development growth guy contain race practice your. Try where where newspaper."
 attribute = "digitalSchematicOverview"
 value = digitalSchematicOverview
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasSchematicOverviewOPDigitalForm = {'type': 'Property', 'value': True}
+hasSchematicOverviewOPDigitalForm = False
 attribute = "hasSchematicOverviewOPDigitalForm"
 value = hasSchematicOverviewOPDigitalForm
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-localRulesOrRestrictions = {'type': 'Property', 'value': True}
+localRulesOrRestrictions = True
 attribute = "localRulesOrRestrictions"
 value = localRulesOrRestrictions
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-localRulesOrRestrictionsDoc = "{'type': 'Property', 'value': 'Road others raise first pay. Pra'}"
+localRulesOrRestrictionsDoc = "Conce"
 attribute = "localRulesOrRestrictionsDoc"
 value = localRulesOrRestrictionsDoc
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
