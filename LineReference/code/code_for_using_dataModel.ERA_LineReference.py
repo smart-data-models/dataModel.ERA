91549db65d26e9f0c3c1e477cb115fb323a8de09
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:32:37 +0200

    created example of code

--- LineReference/code/code_for_using_dataModel.ERA_LineReference.py
+++ LineReference/code/code_for_using_dataModel.ERA_LineReference.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "LineReference"
 subject = "dataModel.ERA"
-kilometer = {'type': 'Property', 'value': 890.1}
+kilometer = 239.9
 attribute = "kilometer"
 value = kilometer
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-lineNationalId = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:LineReference:lineNationalId:HTQW:41563123'}"
+lineNationalId = "urn:ngsi-ld:LineReference:lineNationalId:IIBS:67837023"
 attribute = "lineNationalId"
 value = lineNationalId
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
